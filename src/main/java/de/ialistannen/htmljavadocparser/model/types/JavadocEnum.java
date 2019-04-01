59ca1dd50fc4a129441072986a4fa40f5a38a639
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2019-04-01 21:16:49 +0200

    Allow enums to be generic targets

--- src/main/java/de/ialistannen/htmljavadocparser/model/types/JavadocEnum.java
+++ src/main/java/de/ialistannen/htmljavadocparser/model/types/JavadocEnum.java
@@ -1,12 +1,13 @@
 package de.ialistannen.htmljavadocparser.model.types;
 
+import de.ialistannen.htmljavadocparser.model.properties.GenericTarget;
 import de.ialistannen.htmljavadocparser.model.properties.Nameable;
 import java.util.List;
 
 /**
  * A java enum.
  */
-public interface JavadocEnum extends Type {
+public interface JavadocEnum extends Type, GenericTarget {
 
   /**
    * Returns all enum constants.
