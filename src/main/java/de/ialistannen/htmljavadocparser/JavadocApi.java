7ab9d0fd437db86537625a7eb33a4e33333126ff
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2019-04-02 14:33:30 +0200

    Add a simpler API frontend

---
+++ src/main/java/de/ialistannen/htmljavadocparser/JavadocApi.java
@@ -0,0 +1,58 @@
+package de.ialistannen.htmljavadocparser;
+
+import de.ialistannen.htmljavadocparser.resolving.DocumentResolver;
+import de.ialistannen.htmljavadocparser.resolving.HtmlSummaryParser;
+import de.ialistannen.htmljavadocparser.resolving.Index;
+import de.ialistannen.htmljavadocparser.resolving.ResourcesResolver;
+
+/**
+ * The base class for a Javadoc API.
+ */
+public class JavadocApi {
+
+  private final String baseUrl;
+  private final Index index;
+
+  /**
+   * Creates a new javadoc API and starts indexing.
+   *
+   * @param baseUrl the base url of the API
+   * @param documentResolver the document resolver to use
+   */
+  public JavadocApi(String baseUrl, DocumentResolver documentResolver) {
+    this.baseUrl = baseUrl;
+    this.index = new Index();
+
+    HtmlSummaryParser summaryParser = new HtmlSummaryParser(documentResolver, baseUrl, index);
+    index.addTypes(summaryParser.getTypes());
+    index.addPackages(summaryParser.getPackages());
+  }
+
+  /**
+   * Returns the javadoc index.
+   *
+   * @return the javadoc index
+   */
+  public Index getIndex() {
+    return index;
+  }
+
+  @Override
+  public String toString() {
+    return "JavadocApi{" +
+        "baseUrl='" + baseUrl + '\'' +
+        '}';
+  }
+
+  public static void main(String[] args) {
+    String baseUrl = "https://docs.oracle.com/javase/10/docs/api/";
+    DocumentResolver documentResolver = new ResourcesResolver(
+        baseUrl, "/testfiles"
+    );
+
+    JavadocApi javadocApi = new JavadocApi(baseUrl, documentResolver);
+
+    javadocApi.getIndex().getPackageOrError("java.util").getJavadoc()
+        .ifPresent(System.out::println);
+  }
+}
