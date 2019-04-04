bfcb4f53794df68c7394595279ae8ef62e965e09
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2019-04-04 13:08:46 +0200

    Add a default url document resolver

---
+++ src/main/java/de/ialistannen/htmljavadocparser/resolving/UrlDocumentResolver.java
@@ -0,0 +1,40 @@
+package de.ialistannen.htmljavadocparser.resolving;
+
+import java.io.IOException;
+import java.lang.module.ResolutionException;
+import java.nio.file.Path;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+/**
+ * A {@link DocumentResolver} that connects to the internet.
+ */
+public class UrlDocumentResolver implements DocumentResolver {
+
+  private final String baseUrl;
+
+  /**
+   * The base url.
+   *
+   * @param baseUrl the base url
+   */
+  public UrlDocumentResolver(String baseUrl) {
+    this.baseUrl = baseUrl;
+  }
+
+  @Override
+  public Document resolve(String url) {
+    try {
+      return Jsoup.connect(url).get();
+    } catch (IOException e) {
+      e.printStackTrace();
+      throw new ResolutionException(e);
+    }
+  }
+
+  @Override
+  public String relativizeAbsoluteUrl(String absUrl) {
+    return Path.of(baseUrl).relativize(Path.of(absUrl)).toString();
+  }
+}
+
